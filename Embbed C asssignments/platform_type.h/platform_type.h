9e292f2ef0b620bc4a81fe2061615d041c73e516
Your Name <mohamed>
2024-02-10 01:51:56 +0200

    adding platfornn_type.h

---
+++ Embbed C asssignments/platform_type.h/platform_type.h
@@ -0,0 +1,23 @@
+/*
+ * platform_type.h
+ *
+ *  Created on: Feb 10, 2024
+ *      Author: Lenovo
+ */
+
+#ifndef PLATFORM_TYPE_H_
+#define PLATFORM_TYPE_H_
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long sint64;
+
+typedef float float32;
+
+#endif /* PLATFORM_TYPE_H_ */
